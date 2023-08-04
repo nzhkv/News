4f847b9545d9360388c16d63c56791635c685de8
NZhukov <nzhukov.rnd@gmail.com>
2023-08-04 16:57:18 +0400

    save

--- News/NewsTableViewCell.swift
+++ News/NewsTableViewCell.swift
@@ -7,8 +7,34 @@
 
 import UIKit
 
+class NewsTableViewCellViewModel {
+    let title: String
+    let subtitle : String
+    let imageURL: URL?
+    var imageData: Data?
+    
+    init(title: String, subtitle: String, imageURL: URL?, imageData: Data? = nil) {
+        self.title = title
+        self.subtitle = subtitle
+        self.imageURL = imageURL
+        self.imageData = imageData
+    }
+}
+
 class NewsTableViewCell: UITableViewCell {
     static let identifier = "NewsTableViewCell"
+    
+    private let newsTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 25, weight: .medium)
+        return label
+    }()
+    
+    private let subtitleleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -24,6 +50,10 @@ class NewsTableViewCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+    }
+    
+    func configure(with viewModel: NewsTableViewCellViewModel) {
+        
     }
 
 }
